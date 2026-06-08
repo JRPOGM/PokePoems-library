b7eb7f86077481d0434a09f004492898642d9aa3
JRPOGM <jrmacriss@gmail.com>
2026-06-08 14:28:32 -0700

    making a change

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -58,7 +58,7 @@ class TestTextNode(unittest.TestCase):
     def test_text_two(self):
         node = TextNode("This is not a text node", TextType.BOLD)
         html_node = text_node_to_html_node(node)
-        self.assertEqual(html_node.tag, None)
+        self.assertEqual(html_node.tag, "b")
         self.assertEqual(html_node.value, "This is not a text node")
 
 if __name__ == "__main__":
