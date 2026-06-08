f6deb39a2d31745b8205274e027f2bbbecb0d94e
JRPOGM <jrmacriss@gmail.com>
2026-06-08 13:32:51 -0700

    some html tests

---
+++ src/htmlnode.py
@@ -0,0 +1,22 @@
+
+
+class HTMLNode:
+    def __init__(self, tag: str | None = None, value: str | None = None, children: list["HTMLNode"] | None = None, props: dict[str, str] | None = None):
+        self.tag = tag
+        self.value = value
+        self.children = children
+        self.props = props
+
+    def to_html(self) -> str:
+        raise NotImplementedError("Could not make into html")
+    
+    def props_to_html(self) -> str:
+        if self.props is None:
+            return ""
+        props_html = ""
+        for prop in self.props:
+            props_html += f' {prop}="{self.props[prop]}"'
+        return props_html
+    
+    def __repr__(self) -> str:
+        print(f"HTMLNode(tag={self.tag}, value={self.value}, children={self.children}, props={self.props})")
---
+++ src/test_htmlnode.py
@@ -0,0 +1,29 @@
+import unittest
+
+from htmlnode import HTMLNode
+
+class TestHTMLNode(unittest.TestCase):
+    def test_to_htmp_props(self):
+        node = HTMLNode("div", "Hello, world!", None, {"class": "greeting", "href": "http://boot.dev"},)
+        self.assertEqual(node.props_to_html(), ' class="greeting" href="http://boot.dev:"',)
+
+    def test_value(self):
+        node = HTMLNode("div", "I wish I could read",)
+        self.assertEqual(node.tag, "div",)
+        self.assertEqual(node.value, "I wish I could read",)
+        self.assertEqual(node.children, None,)
+        self.assertEqual(node.props, None,)
+
+    def test_value(self):
+        node = HTMLNode("power", "See what we can",)
+        self.assertNotEqual(node.tag, "strength",)
+        self.assertNotEqual(node.value, "I know where I am",)
+        self.assertEqual(node.children, None,)
+        self.assertEqual(node.props, None,)
+
+    def test_repr(self):
+        node = HTMLNode("p", "What a strange world", None, {"class": "primary"},)
+        self.assertEqual(node.__repr__(), "HTMLNode(p, What a strange world, children: None, {'class': 'primary'})",)
+
+if __name__ == "__main__":
+    unittest.main()
