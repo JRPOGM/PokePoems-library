037536479ff85f8c9c678b6b43837c664f73df6b
JRPOGM <jrmacriss@gmail.com>
2026-06-10 13:20:53 -0700

    a multitest

--- src/test_markdown.py
+++ src/test_markdown.py
@@ -6,6 +6,10 @@ class TestExtractions(unittest.TestCase):
         matches = extract_markdown_images("This is text with an ![image](https://i.imgur.com/zjjcJKZ.png)")
         self.assertListEqual([("image", "https://i.imgur.com/zjjcJKZ.png")], matches)
 
-    def test_markdown_links(self):
+    def test_markdown_link(self):
         matches = extract_markdown_links("This is text with a [link](https://www.youtube.com)")
         self.assertListEqual([("link", "https://www.youtube.com")], matches)
+
+    def test_markdown_links(self):
+        matches = extract_markdown_links("This is text with a [link](https://www.youtube.com) and [another link](https://github.com/JRPOGM/PokePoems-library)")
+        self.assertListEqual([("link", "https://www.youtube.com"), ("another  link", "https://github.com/JRPOGM/PokePoems-library")], matches)
