fb1f9682da28121d643ad98d59ade90d5ab08576
JRPOGM <jrmacriss@gmail.com>
2026-06-12 10:06:54 -0700

    another one

--- src/gencontent.py
+++ src/gencontent.py
@@ -23,6 +23,6 @@ def generate_page(from_path, template_path, dest_path):
     template_content = template_content.replace("{{ Content }}", html)
     destination = os.path.dirname(dest_path)
     if destination != "":
-        os.markdir(destination, exist_ok=True)
+        os.mkdir(destination, exist_ok=True)
     to_file = open(dest_path, "w")
     to_file.write(template_content)
