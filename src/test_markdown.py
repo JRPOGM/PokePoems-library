a2ec3d6d4af3adaaf10e243c93333c90a4de43c0
JRPOGM <jrmacriss@gmail.com>
2026-06-12 09:39:22 -0700

    trying another

--- src/test_markdown.py
+++ src/test_markdown.py
@@ -149,5 +149,10 @@ But this should make it clear
         actual = extract_title("So this shouldn't work if i dont give it a title, right?")
         self.assertNotEqual(actual, "This is just a stand-in")
 
+    def test_title_extract_5(self):
+        actual = extract_title("# Could I write all the titles on one line"
+        "But does this mess anything up?")
+        self.assertEqual(actual, "Could I write all the titles on one line")
+
 if __name__ == "__main__":
     unittest.main()
