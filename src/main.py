043e07e973e24eed98a67f666c5763af60fd0cdb
JRPOGM <jrmacriss@gmail.com>
2026-06-12 09:53:58 -0700

    that should be everything

--- src/main.py
+++ src/main.py
@@ -2,16 +2,22 @@ import os
 import shutil
 
 from copystatic import copy_files_recursive
+from inline_markdown import generate_page
 
-dir_path_static = "./static"
-dir_path_public = "./public"
+direct_path_static = "./static"
+direct_path_public = "./public"
+direct_path_content = "./content"
+template_path = "./template.html"
 
 def main():
     print("Deleting public directory...")
-    if os.path.exists(dir_path_public):
-        shutil.rmtree(dir_path_public)
-
+    if os.path.exists(direct_path_public):
+        shutil.rmtree(direct_path_public)
     print("Copying static files to public directory...")
-    copy_files_recursive(dir_path_static, dir_path_public)
+    copy_files_recursive(direct_path_static, direct_path_public)
+    print("Generating page...")
+    generate_page(os.path.join(direct_path_content, "index.md"), template_path, os.path.join(direct_path_public, "index.html"))
+    
+
 
 main()
