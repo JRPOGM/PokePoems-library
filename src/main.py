18fb5426c8a0f29cd54df181fdac0059eb622b7b
JRPOGM <jrmacriss@gmail.com>
2026-06-12 18:52:49 -0700

    big ass text

--- src/main.py
+++ src/main.py
@@ -31,5 +31,53 @@ def main():
         os.path.join(direct_path_content, "pokemon/squirtle/index.md"),
         template_path,
         os.path.join(direct_path_public, "pokemon/squirtle/index.html"))
+    generate_page(
+        os.path.join(direct_path_content, "pokemon/caterpie/index.md"),
+        template_path,
+        os.path.join(direct_path_public, "pokemon/caterpie/index.html"))
+    generate_page(
+        os.path.join(direct_path_content, "pokemon/weedle/index.md"),
+        template_path,
+        os.path.join(direct_path_public, "pokemon/weedle/index.html"))
+    generate_page(
+        os.path.join(direct_path_content, "pokemon/pidgey/index.md"),
+        template_path,
+        os.path.join(direct_path_public, "pokemon/pidgey/index.html"))
+    generate_page(
+        os.path.join(direct_path_content, "pokemon/rattata/index.md"),
+        template_path,
+        os.path.join(direct_path_public, "pokemon/rattata/index.html"))
+    generate_page(
+        os.path.join(direct_path_content, "pokemon/spearow/index.md"),
+        template_path,
+        os.path.join(direct_path_public, "pokemon/spearow/index.html"))
+    generate_page(
+        os.path.join(direct_path_content, "pokemon/ekans/index.md"),
+        template_path,
+        os.path.join(direct_path_public, "pokemon/ekans/index.html"))
+    generate_page(
+        os.path.join(direct_path_content, "pokemon/sandshrew/index.md"),
+        template_path,
+        os.path.join(direct_path_public, "pokemon/sandshrew/index.html"))
+    generate_page(
+        os.path.join(direct_path_content, "pokemon/nidoranf/index.md"),
+        template_path,
+        os.path.join(direct_path_public, "pokemon/nidoranf/index.html"))
+    generate_page(
+        os.path.join(direct_path_content, "pokemon/nidoranm/index.md"),
+        template_path,
+        os.path.join(direct_path_public, "pokemon/nidoranm/index.html"))
+    generate_page(
+        os.path.join(direct_path_content, "pokemon/vulpix/index.md"),
+        template_path,
+        os.path.join(direct_path_public, "pokemon/vulpix/index.html"))
+    generate_page(
+        os.path.join(direct_path_content, "pokemon/zubat/index.md"),
+        template_path,
+        os.path.join(direct_path_public, "pokemon/zubat/index.html"))
+    generate_page(
+        os.path.join(direct_path_content, "pokemon/oddish/index.md"),
+        template_path,
+        os.path.join(direct_path_public, "pokemon/oddish/index.html"))
 
 main()
