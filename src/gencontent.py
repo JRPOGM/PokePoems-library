738147f6378a62f2b3750a1f7d511c4bfd9abaa7
JRPOGM <jrmacriss@gmail.com>
2026-07-22 12:40:21 -0700

    god please dont mess this up

--- src/gencontent.py
+++ src/gencontent.py
@@ -1,4 +1,5 @@
 import os
+from pathlib import Path
 from block_types import markdown_to_html_node
 
 def extract_title(markdown):
@@ -8,7 +9,18 @@ def extract_title(markdown):
             return line[2:]
     return ValueError("Missing heading")
 
-def generate_page(from_path, template_path, dest_path):
+def generate_pages_recursive(dir_path_content: str, template_path: str, dest_dir_path: str, basepath: str) -> None:
+    for filename in os.listdir(dir_path_content):
+        from_path = os.path.join(dir_path_content, filename)
+        dest_path = os.path.join(dest_dir_path, filename)
+        if os.path.isfile(from_path):
+            dest_path = Path(dest_path).with_suffix(".html")
+            generate_page(from_path, template_path, dest_path, basepath)
+        else:
+            generate_pages_recursive(from_path, template_path, dest_path, basepath)
+
+
+def generate_page(from_path: str, template_path: str, dest_path: str | Path, basepath: str) -> None:
     print(f"Greeting page from {from_path} to {dest_path} using {template_path}.")
     source = open(from_path, "r")
     markdown_file = source.read()
@@ -21,6 +33,8 @@ def generate_page(from_path, template_path, dest_path):
     title = extract_title(markdown_file)
     template_content = template_content.replace("{{ Title }}", title)
     template_content = template_content.replace("{{ Content }}", html)
+    template_content = template_content.replace('href="/', 'href="' + basepath)
+    template_content = template_content.replace('src="/', 'src="' + basepath)
     destination = os.path.dirname(dest_path)
     if destination != "":
         os.makedirs(destination, exist_ok=True)
--- src/main.py
+++ src/main.py
@@ -1,25 +1,28 @@
 import os
 import shutil
+import sys
 
 from copystatic import copy_files_recursive
-from gencontent import generate_page
+from gencontent import generate_pages_recursive
 
 direct_path_static = "./static"
-direct_path_public = "./public"
+direct_path_public = "./docs"
 direct_path_content = "./content"
 template_path = "./template.html"
 template_path_2 = "./template2.html"
+default_basepath = "/"
 
 def main():
+    basepath = default_basepath
+    if len(sys.argv) > 1:
+        basepath = sys.argv[1]
     print("Deleting public directory...")
     if os.path.exists(direct_path_public):
         shutil.rmtree(direct_path_public)
     print("Copying static files to public directory...")
     copy_files_recursive(direct_path_static, direct_path_public)
     print("Generating page...")
-    generate_page(os.path.join(direct_path_content, "index.md"),
-                  template_path,
-                  os.path.join(direct_path_public, "index.html"))
+    generate_pages_recursive(direct_path_content, template_path, direct_path_public, basepath)
     generate_page(
         os.path.join(direct_path_content, "pokemon/bulbasaur/index.md"),
         template_path,
